4fe847e676ea5f3b133524d8a932af591d9e5d63
junzhang93 <mpgszhyh@gmail.com>
2017-05-20 15:26:43 -0700

    修改了一些bug（可能没修好）

--- loc_sha/__init__.py
+++ loc_sha/__init__.py
@@ -2,14 +2,17 @@ from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import create_keyspace_simple
 from cassandra.cqlengine.management import sync_table
 from .cassa_models import Group, Member
+import os
 
-connection.setup(['172.31.4.92'], 'location')
-cluster = connection.cluster
-keyspaces = cluster.metadata.keyspaces
-if 'location' not in keyspaces:
-    create_keyspace_simple('location', replication_factor=2)
-tables = keyspaces['location'].tables
-if 'group' not in tables:
-    sync_table(Group)
-if 'member' not in tables:
-    sync_table(Member)
+if os.getenv('CQLENG_ALLOW_SCHEMA_MANAGEMENT') is None:
+    os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
+    connection.setup(['172.31.4.92'], 'location')
+    cluster = connection.cluster
+    keyspaces = cluster.metadata.keyspaces
+    if 'location' not in keyspaces:
+        create_keyspace_simple('location', replication_factor=2)
+    tables = keyspaces['location'].tables
+    if 'group' not in tables:
+        sync_table(Group)
+    if 'member' not in tables:
+        sync_table(Member)
