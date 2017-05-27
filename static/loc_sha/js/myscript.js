f10e34a149eca0a7d597a757a5832445914fdc0f
junzhang93 <mpgszhyh@gmail.com>
2017-05-27 11:11:11 -0700

    修改Marker的label样式

--- static/loc_sha/js/myscript.js
+++ static/loc_sha/js/myscript.js
@@ -35,7 +35,7 @@ function msgServer() {
             own_marker = new google.maps.Marker({
                 position: {lat: position.coords.latitude, lng: position.coords.longitude},
                 icon: {
-                    labelOrigin: new google.maps.Point(18-4*("TestTest".length), -10),
+                    labelOrigin: new google.maps.Point(18-2*("TestTest".length), -10),
                     url: "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=Y|3FE31A"
                 },
                 label: {
